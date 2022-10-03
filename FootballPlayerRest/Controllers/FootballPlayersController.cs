75ff2fe178bed45339a8815fb1f199a4cd9eed03
Casper Bramming-Hansen <105041881+CBramming@users.noreply.github.com>
2022-10-03 22:25:13 +0200

    Add files via upload

---
+++ FootballPlayerRest/Controllers/FootballPlayersController.cs
@@ -0,0 +1,67 @@
+﻿using Football;
+using FootballPlayerRest.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FootballPlayerRest.Controllers
+{
+    [Route("api/FootballPlayers")]
+    [ApiController]
+    public class FootballPlayersController : ControllerBase
+    {
+        private readonly FootballPlayersManager _manager = new FootballPlayersManager();
+
+        // GET: api/<FootballPlayersController>
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<FootballPlayersController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<FootballPlayer> Get(int id)
+
+        {
+            FootballPlayer fbPlayer = _manager.GetById(id);
+            if (fbPlayer == null) return NotFound("No Such FootballPlayer, id: " + id);
+            return Ok(fbPlayer);
+        }
+
+        // POST api/<FootballPlayersController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPost]
+        public ActionResult<FootballPlayer> Post([FromForm] FootballPlayer value)
+        {
+            FootballPlayer fbPlayer = _manager.Add(value);
+            if (fbPlayer == null) return NotFound("No Such FootballPlayer exist");
+            return Ok(fbPlayer);
+        }
+
+        // PUT api/<FootballPlayersController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPut("{id}")]
+        public ActionResult<FootballPlayer> Put(int id, [FromForm] FootballPlayer value)
+        {
+            FootballPlayer fbPlayer = _manager.Update(id, value);
+            if (fbPlayer == null) return NotFound("ID does not exist, id: " + id);
+            return Ok(fbPlayer);
+        }
+
+        // DELETE api/<FootballPlayersController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id}")]
+        public ActionResult<FootballPlayer> Delete(int id)
+        {
+            FootballPlayer fbPlayer = _manager.Delete(id);
+            if (fbPlayer == null) return NotFound("ID does not exist, id: " + id);
+            return Ok(fbPlayer);
+        }
+    }
+}
---
+++ FootballPlayerRest/Managers/FootballPlayersManager.cs
@@ -0,0 +1,51 @@
+﻿using Football;
+
+namespace FootballPlayerRest.Managers
+{
+    public class FootballPlayersManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<FootballPlayer> Data = new List<FootballPlayer>()
+        {
+            new FootballPlayer {Id = _nextId++, Name = "Casper", Age = 25, ShirtNumber = 7},
+            new FootballPlayer {Id = _nextId++, Name = "Joe", Age = 22, ShirtNumber = 10},
+            new FootballPlayer {Id = _nextId++, Name = "Leo", Age = 24, ShirtNumber = 3},
+            new FootballPlayer {Id = _nextId++, Name = "Tim", Age = 26, ShirtNumber = 1},
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Data);
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Data.Find(fbPlayer => fbPlayer.Id == id);
+        }
+        public FootballPlayer Add(FootballPlayer fbPlayer)
+        {
+            fbPlayer.Id = _nextId++;
+            Data.Add(fbPlayer);
+            return fbPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer fbPlayer = Data.Find(fbPlayer => fbPlayer.Id == id);
+            if (fbPlayer == null) return null;
+            Data.Remove(fbPlayer);
+            return fbPlayer;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer fbPlayer = Data.Find(fbPlayer => fbPlayer.Id == id);
+            if (fbPlayer == null) return null;
+            fbPlayer.Name = updates.Name;
+            fbPlayer.Age = updates.Age;
+            fbPlayer.ShirtNumber = updates.ShirtNumber;
+
+            return fbPlayer;
+        }
+    }
+}
